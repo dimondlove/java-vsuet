17d62a0c7c2e48ed1f803680062bda4d8572b5bb
Yuriy_Rudnev <rudnev2020@gmail.com>
2024-05-29 02:38:15 +0300

    Add eighth task

---
+++ src/eighth/lab/first/ClassApplet.java
@@ -0,0 +1,12 @@
+package eighth.lab.first;
+
+import javax.swing.*;
+import java.applet.Applet;
+import java.awt.*;
+
+public class ClassApplet extends Applet {
+    public void paint (Graphics g)
+    {
+        g.drawString("Hello, XXI century World!!!", 10, 30);
+    }
+}
